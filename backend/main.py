b6ebf367e271602b2ca083f5336003e03086e54a
Joyce Dai <jingyued@uci.edu>
2024-07-29 12:25:51 -0700

    fastapi backend

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+# import and create an app
+from fastapi import FastAPI
+app = FastAPI()
+
+items = []
+
+# define path in fastapi
+@app.get("/")
+def root():
+    return {"Hello":"World"}
+
+# @app.post("/items")
+# def create_item(item:str):
+#     items.append(item)
+#     return items
