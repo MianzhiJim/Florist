4483f5466f0c26288d1afb4fefe7149d3fdafa9e
Joyce Dai <jingyued@uci.edu>
2024-08-06 02:10:45 -0700

    frontend: home page and nav bar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', component: ProductsComponent}
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'home', component: HomeComponent },
+  // { path: 'shop', component: ShopComponent },
+  // { path: 'about-us', component: AboutUsComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'cart', component: CartComponent },
 ];
